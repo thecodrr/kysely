8ba64b74dbef079aaabac473d9218207e4e2d02e
tbui17 <86227911+tbui17@users.noreply.github.com>
2024-02-18 04:26:20 -0700

    Add `LIMIT` operator for update statements (#885)
    * add implementation
    
    * add test

--- src/operation-node/operation-node-transformer.ts
+++ src/operation-node/operation-node-transformer.ts
@@ -554,6 +554,7 @@ export class OperationNodeTransformer {
       returning: this.transformNode(node.returning),
       with: this.transformNode(node.with),
       explain: this.transformNode(node.explain),
+      limit: this.transformNode(node.limit),
     })
   }
 
